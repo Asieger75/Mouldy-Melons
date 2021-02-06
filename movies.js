775cdcff6db007214c2b25a54bafa47dd95a2aa4
MastaKoch <alexkoch1996@gmail.com>
2021-02-06 13:27:54 -0500

    almost done with landing page. then on to styling

---
+++ movies.js
@@ -0,0 +1,3 @@
+// https://api.themoviedb.org/3/movie/550?api_key=bb1f16854dcd8fb0a1e2766615998eea
+// this is an example of a call to the movie db API, with key included.
+// for quality of life, here is documentation for API: https://developers.themoviedb.org/3/getting-started/introduction
